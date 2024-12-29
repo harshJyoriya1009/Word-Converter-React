f2b3a4a555d5b754b7b9512e8092a1e3856c073c
harshJyoriya1009 <harshjyoriyagdg24@gmail.com>
2024-12-29 17:30:23 +0530

    Adding new Button

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 
+
 function App() {
   return (
     <>
     
 
-<Navbar title="Harsh" aboutText="Home"/>
-<div className="container my-3">
-  
-<TextForm heading="Enter your feedback below.."/>
+<Navbar title="Harsh" aboutText="Aditi"/>
+
+<div className="conatiner my-3">
+<TextForm heading="Enter your feedback below"/>
 </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,26 +1,57 @@
+// import React, {useState} from 'react'
+
+// export default function TextForm(props) {
+//     const handleUpClick=()=>{
+//         let nextText= text.toUpperCase()
+//         setText(nextText)
+//     }
+    
+//     const handleOnechange=(event)=>{
+//         setText(event.target.value)
+//     }
+//     const [text, setText] = useState("");
+//   return (
+    
+// <div>
+//   <h1>{props.heading}</h1>
+//   <div className="mb-3">
+//     <textarea className="form-control" value={text} onChange={handleOnechange} id="myBox" rows="9"></textarea>
+//   </div>
+//   <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+//   </div>
+//   )
+// }
+
 import React, {useState} from 'react'
 
+
 export default function TextForm(props) {
-    const handleUpClick=()=>{
-        // console.log("Button is clicked" + text)
-        let nextText= text.toUpperCase()
-        setText(nextText)
-    }
     
-    const handleOnechange=(event)=>{
-        // console.log("Onchange function")
+    const handleOnChange=(event)=>{
         setText(event.target.value)
     }
-    const [text, setText] = useState("");
-    // setText("New one")
-  return (
     
-<div>
-  <h1>{props.heading}</h1>
-  <div className="mb-3">
-    <textarea className="form-control" value={text} onChange={handleOnechange} id="myBox" rows="9"></textarea>
-  </div>
+    const handleUpClick=()=>{
+        let nextText=text.toUpperCase()
+        setText(nextText)
+    }
+
+    const handleDnClick=()=>{
+        let nextText=text.toLowerCase()
+        setText(nextText);
+    }
+    const [text, setText] = useState("");
+  return (
+    <div>
+        <h1>{props.heading}</h1>
+    <div>
+        <div className="mb-3">
+     <textarea className="form-control" value={text} onChange={handleOnChange}  id="myBox" rows="9"></textarea>
+   </div>
   <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-  </div>
+  <button className="btn btn-primary" onClick={handleDnClick}>Convert to lowerCase</button>
+   </div>
+   </div>
+   
   )
 }
