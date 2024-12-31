922f676aa26498b00e80c1650d4ac28e0f2ffeeb
harshJyoriya1009 <harshjyoriyagdg24@gmail.com>
2024-12-31 12:31:34 +0530

    Again some new features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react'
 
 
 function App() {
+
+  const toggleMode=()=>{
+    if(mode==="light"){
+      setMode("dark")
+      document.body.style.backgroundColor="black"   //#212529
+    }else{
+      setMode("light")
+      document.body.style.backgroundColor="white"
+    }
+  }
+
+  const [mode, setMode] = useState('light');
   return (
     <>
     
-
-<Navbar title="Harsh" aboutText="Aditi"/>
+<Navbar title="Harsh" aboutText="Aditi" mode={mode} toggleMode={toggleMode}/>
 
 <div className="container my-3">
-<TextForm heading="Enter your feedback below"/>
+<TextForm heading="Enter your feedback below"  mode={mode} toggleMode={toggleMode} />
 </div>
-<div className="container my-4">
+
+{/* <div className="container my-4">
 <About subHeading1="# Read me 1" subHeading2="# Read me 2" subHeading3="# Read me 3"/>
-</div>
+</div> */}
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -33,18 +33,21 @@ export default function TextForm(props) {
         setText(newText.join(" "))
     }
 
-    const myStyle={
-        color:"black",
-        border: "2px solid black"
-    }
+   
     const [text, setText] = useState("");
   return (
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode==="light"?"black":"white" }}>
         <h1>{props.heading}</h1>
     <div>
         <div className="mb-3">
-     <textarea className="form-control" value={text} onChange={handleOnChange}  id="myBox" rows="9"></textarea>
+     <textarea className="form-control" value={text} onChange={handleOnChange} 
+     style={
+        {
+            backgroundColor: props.mode==="light"?"white":"gray",              //onject inside js
+             color:  props.mode==="light"?"black":"white"
+              }
+              } id="myBox" rows="9"></textarea>
    </div>
   <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to uppercase</button>
   <button className="btn btn-primary mx-1 my-1" onClick={handleDnClick}>Convert to lowerCase</button>
@@ -53,16 +56,14 @@ export default function TextForm(props) {
   <button className="btn btn-primary mx-1 my-1 " onClick={removeExtraSpaces}>Remove Extra Spaces</button>
    </div>
    </div>
-   <div className="container my-2">
+
+   <div className="container my-2" style={{color: props.mode==="light"?"black":"white" }}>
     <h2>Your text summary----------------</h2>
     <p>{text.split(" ").length} words and {text.length} characters</p>
     <p>{0.008 * text.split(" ").length} Minutes to read</p>
-
-    <div className="xod my-3"style={myStyle}>
-    <h2 className='container' >Preview</h2>
-    <p className='container'>{text}</p>
-    </div>
-
+    <h2>Preview</h2>
+    <p >{text.length>0?text:"Enter something"}</p>
+    
    </div>
 
    </>
